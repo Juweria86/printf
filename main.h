524358431c5f23b9bc02cca958513b7043a62f1e
Juweria86 <jujumelmi143@gmail.com>
2023-08-20 17:46:32 +0300

    solving errors on task1

--- main.h
+++ main.h
@@ -3,6 +3,5 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _putchar(char c);
 int _printf(const char *format, ...);
 #endif
---
+++ print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number -prints integers.
+ * @n: an integer
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
