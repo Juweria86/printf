79a8fe5d78a6c6fc1bf5ab80fa29655617e538a5
Juweria86 <jujumelmi143@gmail.com>
2023-08-20 18:11:02 +0300

    solving errors

--- handlers.c
+++ handlers.c
@@ -1,42 +1,41 @@
 #include "main.h"
 /**
- * _strlen -function that returns the length of a string.
- * @s: character
- * Return: length
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-/**
  *  _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 /**
- * print_string  - prints string
- * @s: string to be printed
- * Return: prints length of string
+ * print_number -prints integers.
+ * @n: an integer
+ * Return: i
  */
-int print_string(char *s)
+
+int print_number(int n)
 {
-	if (s == NULL)
-		s = "(null)";
-	return (write(1, s,  _strlen(s)));
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+	return (i);
 }
+
 /**
  * _printf - produces output according to a format
  * @format: character string
@@ -63,21 +62,14 @@ int _printf(const char *format, ...)
 				return (-1);
 			switch (*format)
 			{
-
-				case '%':
-					print_char += _putchar('%');
-					break;
-				case 'c':
-					print_char += _putchar(va_arg(args, int));
-					break;
-				case 's':
-
-					print_char += print_string(va_arg(args, char *));
+				case 'd':
+				case 'i':
+					print_char += print_number(va_arg(args, int));
 					break;
 				default:
 					print_char += _putchar('%');
 					print_char += _putchar(*format);
-					break;
+					break;				
 			}
 		}
 		format++;
