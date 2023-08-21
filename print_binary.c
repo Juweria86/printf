b608300672e52fe3a1b9005cb35576144550b599
Juweria86 <jujumelmi143@gmail.com>
2023-08-21 18:24:29 +0300

    solving some requirement errors

--- print_binary.c
+++ print_binary.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
  * print_binary - prints binary of unsigned int
- * @n: an integer to be printed as binary
+ * @b: an integer to be converted to binary
  */
-void print_binary(unsigned int n)
+void print_binary(unsigned int b)
 {
 	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
 	int leading_zeros = 1;
 
-	if (n == 0)
+	if (b == 0)
 	{
 		_putchar('0');
 		return;
 	}
 	while (mask != 0)
 	{
-		if ((n & mask) != 0)
+		if ((b & mask) != 0)
 		{
 			leading_zeros = 0;
 			_putchar('1');
