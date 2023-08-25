84c7959e24317d4148ae215192b414ddd0a651bd
Juweria86 <jujumelmi143@gmail.com>
2023-08-26 01:18:17 +0300

    updating flags.c

--- flags.c
+++ flags.c
@@ -80,15 +80,15 @@ int get_precision(const char *format, int *i, va_list args)
  */
 int get_size(const char *format, int *i)
 {
-        int currentIndex = *i + 1;
-        int size = 0;
+	int currentIndex = *i + 1;
+	int size = 0;
 
-        if (format[currentIndex] == 'l')
-                size = S_LONG;
-        else if (format[currentIndex] == 'h')
-                size = S_SHORT;
-        *i = (size != 0) ? currentIndex : currentIndex - 1;
-        return (size);
+	if (format[currentIndex] == 'l')
+		size = S_LONG;
+	else if (format[currentIndex] == 'h')
+		size = S_SHORT;
+	*i = (size != 0) ? currentIndex : currentIndex - 1;
+	return (size);
 }
 /**
  * get_width - Calculates the width for printing
@@ -99,28 +99,28 @@ int get_size(const char *format, int *i)
  */
 int get_width(const char *format, int *i, va_list args)
 {
-        int currentIndex = *i + 1;
-        int width = 0;
+	int currentIndex = *i + 1;
+	int width = 0;
 
-        while (format[currentIndex] != '\0')
-        {
-                if (is_digit(format[currentIndex]))
-                {
-                        width *= 10;
-                        width += format[currentIndex] - '0';
-                }
-                else if (format[currentIndex] == '*')
-                {
-                        currentIndex++;
-                        width = va_arg(list, int);
-                        break;
-                }
-                else
-                {
-                        break;
-                }
-                currentIndex++;
-        }
-        *i = currentIndex - 1;
-        return (width);
+	while (format[currentIndex] != '\0')
+	{
+		if (is_digit(format[currentIndex]))
+		{
+			width *= 10;
+			width += format[currentIndex] - '0';
+		}
+		else if (format[currentIndex] == '*')
+		{
+			currentIndex++;
+			width = va_arg(args, int);
+			break;
+		}
+		else
+		{
+			break;
+		}
+		currentIndex++;
+	}
+	*i = currentIndex - 1;
+	return (width);
 }
