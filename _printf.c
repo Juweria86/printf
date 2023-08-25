aa3a6bd832aba0d8afad022fb04057923608e396
Juweria86 <jujumelmi143@gmail.com>
2023-08-25 13:55:23 +0300

    correcting some tasks

--- _printf.c
+++ _printf.c
@@ -1,49 +1,59 @@
 #include "main.h"
 /**
- * _printf - produces output according to a format
- * @format: character string
- * Return: number of chracters printed
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_index: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_index)
+{
+	if (*buff_index > 0)
+		write(1, &buffer[0], *buff_index);
+
+	*buff_index = 0;
+}
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
+	int i, printed = 0, char_print = 0;
+	int flags, width, precision, size, buff_index = 0;
 	va_list args;
-	int char_print = 0;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	while (*format)
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*format != '%')
-			char_print += _putchar(*format);
+		if (format[i] != '%')
+		{
+			buffer[buff_index++] = format[i];
+			if (buff_index == BUFF_SIZE)
+				print_buffer(buffer, &buff_index);
+			char_print++;
+		}
 		else
 		{
-			format++;
-			if (*format == '\0')
+			print_buffer(buffer, &buff_index);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, args, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			switch (*format)
-			{
-				case '%':
-					char_print+= _putchar('%');
-					break;
-				case 'c':
-					char_print += _putchar(va_arg(args, int));
-					break;
-				case 's':
-<<<<<<< HEAD
-					print_char += print_string(va_arg(args, char *));
-=======
-					char_print += print_string(va_arg(args, char *));
->>>>>>> origin/main
-					break;
-				default:
-					char_print += _putchar('%');
-					char_print += _putchar(*format);
-					break;
-			}
+			char_print += printed;
 		}
-		format++;
 	}
+
+	print_buffer(buffer, &buff_index);
 	va_end(args);
-	return (print_char);
+	return (char_print);
 }
+
---
+++ flags.c
@@ -0,0 +1,123 @@
+#include "main.h"
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int get_flags(const char *format, int *i)
+{
+	int currentIndex = *i + 1;
+	int flags = 0, j;
+	const char flagCharacters[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int flagValues[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	while (format[currentIndex] != '\0')
+	{
+		for (j = 0; flagCharacters[j] != '\0'; j++)
+		{
+			if (format[currentIndex] == flagCharacters[j])
+			{
+				flags |= flagValues[j];
+				break;
+			}
+		}
+		if (flagCharacters[j] == 0)
+			return (flags);
+		currentIndex++;
+	}
+	*i = currentIndex - 1;
+	return (flags);
+}
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+        int currentIndex = *i + 1;
+        int precision = -1;
+
+        if (format[currentIndex] != '.')
+                return (precision);
+        precision = 0;
+        while (format[currentIndex] != '\0')
+        {
+                if (is_digit(format[currentIndex]))
+                {
+                        precision *= 10;
+                        precision += format[currentIndex] - '0';
+                }
+                else if (format[currentIndex] == '*')
+                {
+                        currentIndex++;
+                        precision = va_arg(list, int);
+                        break;
+                }
+                else
+                {
+                        break;
+                }
+                currentIndex++;
+        }
+        *i = currentIndex - 1;
+        return (precision);
+}
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+        int currentIndex = *i + 1;
+        int size = 0;
+
+        if (format[currentIndex] == 'l')
+                size = S_LONG;
+        else if (format[currentIndex] == 'h')
+                size = S_SHORT;
+        *i = (size != 0) ? currentIndex : currentIndex - 1;
+        return (size);
+}
+/**
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+        int currentIndex = *i + 1;
+        int width = 0;
+
+        while (format[currentIndex] != '\0')
+        {
+                if (is_digit(format[currentIndex]))
+                {
+                        width *= 10;
+                        width += format[currentIndex] - '0';
+                }
+                else if (format[currentIndex] == '*')
+                {
+                        currentIndex++;
+                        width = va_arg(list, int);
+                        break;
+                }
+                else
+                {
+                        break;
+                }
+                currentIndex++;
+        }
+        *i = currentIndex - 1;
+        return (width);
+}
